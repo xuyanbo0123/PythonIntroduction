60305f22ae62f1e6e5448e035abd96aabe1b5f62
Calmadness <calmadness@gmail.com>
2017-07-26 14:18:38 -0500

    first commit

---
+++ lesson5/task2/boolean_order.py
@@ -0,0 +1,6 @@
+name = "John"
+age = 17
+
+print(name == "John" or not age > 17)
+
+print("name" is "Ellis" or not ("name" == "John" and age == 17))
---
+++ lesson9/task2/builtin_modules.py
@@ -0,0 +1,3 @@
+import datetime
+
+print(datetime.date(2015,7,3))
